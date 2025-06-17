858f7b2803e49e2231954a9bf7f525a4c7d250eb
ivanterner <ivanterner@gmail.com>
2025-06-17 14:59:43 +0000

    endpoint

--- web_app/app.py
+++ web_app/app.py
@@ -1,12 +1,32 @@
-import time 
 import redis 
-from flask import Flask 
+from flask import Flask, make_response 
+import socket 
+
 app = Flask(__name__) 
-cache = redis.Redis(host='redis', port=6379)
-def get_hit_count(): 
-	return cache.incr('hits') 
+cache = redis.Redis(host='redis', port=6379) 
+
+def get_hit_count() -> int: 
+    return int(cache.get('hits') or 0) 
+
+def incr_hit_count() -> int: 
+    return cache.incr('hits') 
+
+
+@app.route('/metrics') 
+
+def metrics(): 
+    metrics = f''' 
+    # HELP view_count Flask-Redis-App visit counter 
+    # TYPE view_count counter 
+    view_count{{service="Flask-Redis-App"}} {get_hit_count()} 
+    ''' # sic double quotes in label 
+    response = make_response(metrics, 200) 
+    response.mimetype = "text/plain" 
+    return response 
+
 
 @app.route('/') 
 def hello(): 
-	count = get_hit_count() 
-	return 'Hello World! I have been seen {} times.\n'.format(count)
+    incr_hit_count() 
+    count = get_hit_count() 
+    return 'Hello World! I have been seen {} times. My name is: {}\n'.format(count, socket.gethostname())
